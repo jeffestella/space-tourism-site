fc740daf936b0a52cf06ebfcef4a6de07e9ed4f7
Jeff <jeffrey.estella@gmail.com>
2022-03-04 18:50:56 -0800

    Add default background and color style to index.css. Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import navbarStyles from './Navbar.module.css';
+
+const Navbar = () => {
+    return (
+        <header>
+            Header
+        </header>
+    );
+}
+
+export default Navbar;
