0a6d5e01584bed737973d25624d6306b55799733
Jeff <jeffrey.estella@gmail.com>
2022-03-04 20:04:07 -0800

    Begin building Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import navbarStyles from './Navbar.module.css';
+import React from "react";
+import navbarStyles from "./Navbar.module.css";
+
+import logo from "../assets/shared/logo.svg";
+import hamburger from "../assets/shared/icon-hamburger.svg";
 
 const Navbar = () => {
     return (
-        <header>
-            Header
+        <header className={navbarStyles.navBarContainer}>
+            <img src={logo} alt="logo" />
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Destination</a></li>
+                    <li><a href="">Crew</a></li>
+                    <li><a href="">Technology</a></li>
+                </ul>
+            </nav>
+            <button>
+                <img src={hamburger} alt="menu toggle button" />
+            </button>
         </header>
     );
 }
