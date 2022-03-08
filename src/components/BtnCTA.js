d902dbf6c9031d61f38017442e1da79a8b704dfb
Jeff <jeffrey.estella@gmail.com>
2022-03-07 19:57:42 -0800

    Add BtnCTA component. Finish mobile styling for BtnCTA and SectionHero

---
+++ src/components/BtnCTA.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import btnCTAStyles from './BtnCTA.module.css';
+
+const BtnCTA = () => {
+    return (
+        <button className={btnCTAStyles.btn}>EXPLORE</button>
+    );
+}
+
+export default BtnCTA;
--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import sectionHeroStyles from './SectionHero.module.css';
 
+import BtnCTA from './BtnCTA';
+
 const SectionHero = () => {
     return (
         <section className={sectionHeroStyles.heroContainer}>
@@ -9,6 +11,7 @@ const SectionHero = () => {
                 <p className={sectionHeroStyles.heroHeadlineSpace}>SPACE</p>
                 <p className={sectionHeroStyles.heroBodyCopy}>Let's face it; if you want to go to space, you might as well genuinely go to outer space and nt hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
             </div>
+            <BtnCTA />
         </section>
     );
 }
