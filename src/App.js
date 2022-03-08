c0f9f894418fa9b626d26863916f2ca7c94dce10
Jeff <jeffrey.estella@gmail.com>
2022-03-07 19:47:42 -0800

    Add SectionHero component and style its headline and copy

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import appStyles from './App.module.css';
 
 import Navbar from './components/Navbar';
+import SectionHero from './components/SectionHero';
 
 const App = () => {
     return (
         <div className={appStyles.appContainer}>
             <Navbar />
+            <SectionHero />
         </div>
     );
 }
