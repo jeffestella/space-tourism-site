2cd934a04ffc8967c642f793ac3bf17d687c25ee
Jeff <jeffrey.estella@gmail.com>
2022-03-07 19:47:24 -0800

    Add SectionHero component and style its headline and copy

---
+++ src/components/SectionHero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import sectionHeroStyles from './SectionHero.module.css';
+
+const SectionHero = () => {
+    return (
+        <section className={sectionHeroStyles.heroContainer}>
+            <div className={sectionHeroStyles.heroCopy}>
+                <p className={sectionHeroStyles.heroHeadline}>So, you want to travel to</p>
+                <p className={sectionHeroStyles.heroHeadlineSpace}>SPACE</p>
+                <p className={sectionHeroStyles.heroBodyCopy}>Let's face it; if you want to go to space, you might as well genuinely go to outer space and nt hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+            </div>
+        </section>
+    );
+}
+
+export default SectionHero;
