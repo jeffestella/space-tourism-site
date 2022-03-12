5777a427cd5e9e9b130627a5e0fd6642216ca410
Jeff <jeffrey.estella@gmail.com>
2022-03-11 20:49:03 -0800

    Start building Destination page

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,10 +1,16 @@
 import React from "react";
 import destinationStyles from './Destination.module.css';
 
+import imageMoon from "../assets/destination/image-moon.png";
+
 const Destination = () => {
     return (
-        <main className={destinationStyles.main}>
-            Destination Page
+        <main className={destinationStyles.destinationMain}>
+            <section>
+                <p>01 Pick Your Destination</p>
+                <img src={imageMoon} alt="photo of moon"/>
+                <p>Moon</p>
+            </section>
         </main>
     );
 }
