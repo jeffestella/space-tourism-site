2fb5c6ae42b607443bfabc2605b102b0f39253ac
Jeff <jeffrey.estella@gmail.com>
2022-03-12 09:10:14 -0800

    Finish HTML for Destination page. Begin styling Destination page

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -6,12 +6,30 @@ import imageMoon from "../assets/destination/image-moon.png";
 const Destination = () => {
     return (
         <main className={destinationStyles.destinationMain}>
-            <section>
-                <p>01 Pick Your Destination</p>
-                <img src={imageMoon} alt="photo of moon"/>
-                <p>Moon</p>
-                <p>See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there.</p>
-            </section>
+                <h1 className={destinationStyles.headerPick}>01 Pick Your Destination</h1>
+                <figure className={destinationStyles.photoSelect}>
+                    <img src={imageMoon} alt="photo of moon" className={destinationStyles.photoSelectImg}/>
+                    <figcaption className={destinationStyles.photoSelectCaption}>
+                        <ul className={destinationStyles.photoSelectMenu}>
+                            <li className={destinationStyles.photoSelectMenuItem}><a href="" className={destinationStyles.photoSelectMenuLink}>Moon</a></li>
+                            <li className={destinationStyles.photoSelectMenuItem}><a href="" className={destinationStyles.photoSelectMenuLink}>Mars</a></li>
+                            <li className={destinationStyles.photoSelectMenuItem}><a href="" className={destinationStyles.photoSelectMenuLink}>Europa</a></li>
+                            <li className={destinationStyles.photoSelectMenuItem}><a href="" className={destinationStyles.photoSelectMenuLink}>Titan</a></li>
+                        </ul>
+                    </figcaption>
+                </figure>
+                <section className={destinationStyles.card}>
+                    <header className={destinationStyles.cardName}>Moon</header>
+                    <p className={destinationStyles.cardBody}>See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there.</p>
+                </section>
+                <hr />
+                <section className={destinationStyles.stats}>
+                    <h2 className={destinationStyles.statsMetric}>Avg. Distance</h2>
+                    <p className={destinationStyles.statsValue}>384,400KM</p>
+                    <h2>Est. Travel Time</h2>
+                    <p>3 Days</p>
+                </section>
+
         </main>
     );
 }
