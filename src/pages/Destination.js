0642ebdf19f1f0727d5dd634eda84e9522d54b8f
Jeff <jeffrey.estella@gmail.com>
2022-03-15 19:58:09 -0700

    Finish Destination page component menu and text content functionality

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -7,63 +7,88 @@ import imageEuropa from "../assets/destination/image-europa.png";
 import imageTitan from "../assets/destination/image-titan.png";
 
 const Destination = () => {
-    const destList = {
-        moon: {
+    const destData = [
+        {
             name: "MOON",
             blurb: "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting Luna 2 and Apollo 11 landing sites.",
             distance: "384,400 KM",
             travelTime: "3 DAYS",
-            photo: imageMoon
+            photo: {
+                file: imageMoon,
+                alt: "photo of moon"
+            }
         },
-        mars: {
+        {
             name: "MARS",
             blurb: "Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!",
             distance: "225 MIL. KM",
             travelTime: "9 MONTHS",
-            photo: imageMars
+            photo: {
+                file: imageMars,
+                alt: "photo of mars"
+            }
         },
-        europa: {
+        {
             name: "EUROPA",
             blurb: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream. With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
             distance: "628 MIL. KM",
             travelTime: "3 YEARS",
-            photo: imageEuropa
+            photo: {
+                file: imageEuropa,
+                alt: "photo of europa"
+            }
         },
-        titan: {
+        {
             name: "TITAN",
             blurb: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn",
             distance: "1.6 BIL. KM",
             travelTime: "7 YEARS",
-            photo: imageTitan
+            photo: {
+                file: imageTitan,
+                alt: "photo of titan"
+            }
         }
-    }
+    ]
 
-    const [currentDest, setCurrentDest] = useState(destList.moon);
+    const destMenu = destData.map((dest) => {
+        return (
+            <li 
+                className={destinationStyles.photoSelectMenuItem}
+                onClick={() => setCurrentDest(dest)}
+                key={dest.name}
+            >
+                {dest.name}
+            </li>
+        )
+    })
+
+    const [currentDest, setCurrentDest] = useState(destData[0]);
 
     return (
         <main className={destinationStyles.destinationMain}>
                 <h1 className={destinationStyles.headerPick}><span className={destinationStyles.headerPickIndex}>01</span> PICK YOUR DESTINATION</h1>
                 <figure className={destinationStyles.photoSelect}>
-                    <img src={imageMoon} alt="photo of moon" className={destinationStyles.photoSelectImg}/>
+                    <img src={currentDest.photo.file} alt={currentDest.photo.alt} className={destinationStyles.photoSelectImg}/>
                     <figcaption className={destinationStyles.photoSelectCaption}>
                         <ul className={destinationStyles.photoSelectMenu}>
-                            <li className={destinationStyles.photoSelectMenuItem}><a href="" className={destinationStyles.photoSelectMenuLink}>Moon</a></li>
-                            <li className={destinationStyles.photoSelectMenuItem}><a href="" className={destinationStyles.photoSelectMenuLink}>Mars</a></li>
-                            <li className={destinationStyles.photoSelectMenuItem}><a href="" className={destinationStyles.photoSelectMenuLink}>Europa</a></li>
-                            <li className={destinationStyles.photoSelectMenuItem}><a href="" className={destinationStyles.photoSelectMenuLink}>Titan</a></li>
+                            {destMenu}
                         </ul>
                     </figcaption>
                 </figure>
                 <section className={destinationStyles.card}>
-                    <header className={destinationStyles.cardName}>Moon</header>
-                    <p className={destinationStyles.cardBody}>See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there.</p>
+                    <header className={destinationStyles.cardName}>
+                        {currentDest.name}
+                    </header>
+                    <p className={destinationStyles.cardBody}>
+                        {currentDest.blurb}
+                    </p>
                 </section>
                 <hr />
                 <section className={destinationStyles.stats}>
                     <h2 className={destinationStyles.statsMetric}>Avg. Distance</h2>
-                    <p className={destinationStyles.statsValue}>384,400KM</p>
+                    <p className={destinationStyles.statsValue}>{currentDest.distance}</p>
                     <h2>Est. Travel Time</h2>
-                    <p>3 Days</p>
+                    <p>{currentDest.travelTime}</p>
                 </section>
 
         </main>
