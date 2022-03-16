fdaede2b4113b11e4cb2f58c9cc14fdc0559db71
Jeff <jeffrey.estella@gmail.com>
2022-03-15 19:19:54 -0700

    Add photos to destList for Destination page

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import destinationStyles from './Destination.module.css';
 
 import imageMoon from "../assets/destination/image-moon.png";
+import imageMars from "../assets/destination/image-mars.png";
+import imageEuropa from "../assets/destination/image-europa.png";
+import imageTitan from "../assets/destination/image-titan.png";
 
 const Destination = () => {
     const destList = {
@@ -9,25 +12,29 @@ const Destination = () => {
             name: "MOON",
             blurb: "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting Luna 2 and Apollo 11 landing sites.",
             distance: "384,400 KM",
-            travelTime: "3 DAYS"
+            travelTime: "3 DAYS",
+            photo: imageMoon
         },
         mars: {
             name: "MARS",
             blurb: "Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!",
             distance: "225 MIL. KM",
-            travelTime: "9 MONTHS"
+            travelTime: "9 MONTHS",
+            photo: imageMars
         },
         europa: {
             name: "EUROPA",
             blurb: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream. With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
             distance: "628 MIL. KM",
-            travelTime: "3 YEARS"
+            travelTime: "3 YEARS",
+            photo: imageEuropa
         },
         titan: {
             name: "TITAN",
             blurb: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn",
             distance: "1.6 BIL. KM",
-            travelTime: "7 YEARS"
+            travelTime: "7 YEARS",
+            photo: imageTitan
         }
     }
 
