967a7bb04b50ae6a12fca71b46e89235eb5a8c32
Jeff <jeffrey.estella@gmail.com>
2022-04-01 20:03:59 -0700

    Update Navbar menu styling to dark translucent background. Add JSX logic for currentPage state and class for Navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,50 @@ import logo from "../assets/shared/logo.svg";
 
 
 const Navbar = () => {
+    const pages = [
+        {
+            name: "Home",
+            slug:"/"
+        },
+        {
+            name: "Destination",
+            slug:"/destination"
+        },
+        {
+            name: "Crew",
+            slug:"/crew"
+        },
+        {
+            name: "Technology",
+            slug:"/technology"
+        },
+    ]
+
+    const [currentPage, setCurrentPage] = useState(pages[0])
     const [navOpen, setNavOpen] = useState(false);
+
+    const navLinks = pages.map((page) => {
+        return <li className={navbarStyles.navListItem}>
+            <Link
+                to={page.slug}
+                className={`${navbarStyles.navListLink} ${
+                    currentPage.name === page.name ?
+                    navbarStyles.currentPage : ""}`}
+                href=""
+                onClick={() => {
+                    setCurrentPage(page);
+                    setNavOpen(false)}}
+            >{page.name}</Link>
+        </li>
+    })
+
     return (
         <header className={navbarStyles.navbarContainer}>
             <img src={logo} alt="logo" />
             <nav className={`${navOpen ? navbarStyles.navbar : navbarStyles.hidden }`}>
                 <ul className={navbarStyles.navList}>
-                    <li className={navbarStyles.navListItem}>
+                    {navLinks}
+                    {/* <li className={navbarStyles.navListItem}>
                         <Link 
                             to="/" 
                             className={navbarStyles.navListLink} 
@@ -43,7 +80,7 @@ const Navbar = () => {
                             href=""
                             onClick={() => {setNavOpen(false)}}>Technology
                         </Link>
-                    </li>
+                    </li> */}
                 </ul>
             </nav>
             <BtnNavToggle 
